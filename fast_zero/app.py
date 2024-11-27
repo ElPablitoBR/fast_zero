d0a2641d727d3df91c3c1e15f99597ea38c2d8b4
Pablo Batista <pablobatista@outlook.com>
2024-11-27 01:19:30 -0300

    exibindo o htmlcov na minha aplicação. http://127.0.0.1:8000/coverage/index.html

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,49 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from fastapi.responses import FileResponse
+
+import os
+
+from fastapi.staticfiles import StaticFiles
+
+from fast_zero.schemas import Message, UserDB, UserPublic, UserSchema
 
 app = FastAPI()
 
+database = []
 
-@app.get('/')
+
+@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'message': 'Olá Mundo!'}
+    return {"message": "Olá Mundo!"}
+
+
+@app.post("/users/", status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
+
+    database.append(user_with_id)
+    return user_with_id
+
+
+# Verifica se o diretório de cobertura existe
+if os.path.exists("htmlcov"):
+    app.mount("/coverage", StaticFiles(directory="htmlcov"), name="coverage")
+else:
+    print(
+        "Diretório htmlcov não encontrado. Execute `task test` para gerar o relatório."
+    )
+
+
+@app.get("/coverage/{file_path:path}")
+async def serve_coverage_files(file_path: str):
+    """
+    Serve arquivos estáticos do diretório htmlcov.
+    """
+    base_path = "htmlcov"
+    file_location = os.path.join(base_path, file_path)
+    if os.path.exists(file_location):
+        return FileResponse(file_location)
+    return {"error": "File not found"}
--- tests/test_app.py
+++ tests/test_app.py
@@ -8,7 +8,27 @@ from fast_zero.app import app
 def test_root_deve_retornar_ok_e_ola_mundo():
     client = TestClient(app)  # Arrange
 
-    response = client.get('/')  # Act
+    response = client.get("/")  # Act
 
     assert response.status_code == HTTPStatus.OK  # Assert
-    assert response.json() == {'message': 'Olá Mundo!'}  # Assert
+    assert response.json() == {"message": "Olá Mundo!"}  # Assert
+
+
+def test_create_user():
+    client = TestClient(app)  # Arrange
+    response = client.post(  # UserSchema
+        "/users/",
+        json={
+            "username": "testeusername",
+            "password": "senhadificil",
+            "email": "teste@teste.com",
+        },
+    )
+    # Voltou o status code correto?
+    assert response.status_code == HTTPStatus.CREATED
+    # Validar UserPublic
+    assert response.json() == {
+        "username": "testeusername",
+        "email": "teste@teste.com",
+        "id": 1,
+    }
